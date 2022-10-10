e2194e6bbd8dd19f1536198be726fb78651de041
Agustin Escribano <aguescribano87@gmail.com>
2022-10-10 10:02:43 -0300

    clear

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,18 +1,14 @@
-import { useContext } from "react"
-import { DisplayContext } from "../context/displayContext"
 
 const Contador = () => {
-  const [store, dispatch] = useContext(DisplayContext)
-  const { display } = store
   return (
     <>
-      <span className='nameDisplay'>{display}</span>
+      <span className='nameDisplay'>0</span>
       <div className='containerButtons'>
-        <button onClick={() => dispatch({ type: 'RESTAR_CINCO', payload: 5 })}> -5 </button>
-        <button onClick={() => dispatch({ type: 'RESTAR_UNO' })}> -1 </button>
-        <button onClick={() => dispatch({ type: 'RESET' })}> Reset </button>
-        <button onClick={() => dispatch({ type: 'SUMAR_UNO' })}> +1 </button>
-        <button onClick={() => dispatch({ type: 'SUMAR_CINCO', payload: 5 })}> +5 </button>
+        <button> -5 </button>
+        <button> -1 </button>
+        <button> Reset </button>
+        <button> +1 </button>
+        <button> +5 </button>
       </div>
     </>
   )
--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { DisplayProvider } from './context/displayContext';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <DisplayProvider>
     <App />
-  </DisplayProvider>
 )
