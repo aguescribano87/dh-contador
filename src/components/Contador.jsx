f7f52e115962dc974ca9aee5f8023058fd3f192f
Agustin Escribano <aguescribano87@gmail.com>
2022-10-19 20:31:18 -0300

    C2S

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,19 +1,35 @@
+import { useRef } from "react"
+import { useEffect } from "react"
 import { useContext } from "react"
 import { DisplayContext } from "../context/displayContext"
 
 const Contador = () => {
-  const [display, setDisplay] = useContext(DisplayContext)
+  const [store, dispatch] = useContext(DisplayContext)
+  const miRef = useRef(null)
+  const refValue = useRef(0)
+
+  const click = ()=> miRef.current.focus()
+  
+  useEffect(()=>{
+    if (refValue.current == 0) {
+      refValue.current = refValue.current +1
+      return
+    }
+    console.log(`El estado cambio y su valor es ${store}`)
+  },[store])
 
   return (
     <>
-      <span className='nameDisplay'>{display}</span>
+      <span className='nameDisplay'>{store}</span>
       <div className='containerButtons'>
-        <button onClick={() => setDisplay(display - 5)}> -5 </button>
-        <button onClick={() => setDisplay(display - 1)}> -1 </button>
-        <button onClick={() => setDisplay(0)}> Reset </button>
-        <button onClick={() => setDisplay(display + 1)}> +1 </button>
-        <button onClick={() => setDisplay(display + 5)}> +5 </button>
+        <button onClick={() => dispatch({ type: 'RESTAR_CINCO', payload: 5 })}> -5 </button>
+        <button onClick={() => dispatch({ type: 'RESTAR_UNO' })}> -1 </button>
+        <button onClick={() => dispatch({ type: 'RESET' })}> Reset </button>
+        <button onClick={() => dispatch({ type: 'SUMA_UNO' })}> +1 </button>
+        <button onClick={() => dispatch({ type: 'SUMAR_CINCO', payload: 5 })}> +5 </button>
       </div>
+      <input ref={miRef}/>
+      <button onClick={click}>Click</button>
     </>
   )
 }
--- src/context/displayContext.jsx
+++ src/context/displayContext.jsx
@@ -1,13 +1,35 @@
-import { createContext, useState } from "react";
+import { useReducer } from "react";
+import { createContext } from "react";
 
 export const DisplayContext = createContext()
 
-export const DisplayProvider = ({ children }) => {
-    const [display, setDisplay] = useState(0)
+const initialState = 0
 
+const displayReducer = (state, action) => {
+    switch (action.type) {
+        case 'SUMA_UNO':
+            return ++state
+            break;
+        case 'RESTAR_UNO':
+            return --state
+            break;
+        case 'RESET':
+            return initialState
+            break;
+        case 'RESTAR_CINCO':
+            return state - action.payload
+            break;
+        case 'SUMAR_CINCO':
+            return state + action.payload
+            break;
+    }
+}
+
+export const DisplayProvider = ({ children }) => {
+    const [store, dispatch] = useReducer(displayReducer, initialState)
 
     return (
-        <DisplayContext.Provider value={[display, setDisplay]}>
+        <DisplayContext.Provider value={[store, dispatch]}>
             {children}
         </DisplayContext.Provider>
     )
